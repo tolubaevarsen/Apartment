9d038d0bf86743ee00619b36075bd52b167d521f
tolubaevarsen <tolubaevarsen7@gmail.com>
2022-11-17 21:43:18 +0600

    account

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def _create(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('User must have username!')
+        if not email:
+            raise ValueError('User must have email!')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_active', False)
+        return self._create(username, email, password, extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        return self._create(username, email, password, extra_fields)
+
