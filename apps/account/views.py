9550f01516cc0860c88003c1fe7d97e8bf48ff4d
tolubaevarsen <tolubaevarsen7@gmail.com>
2022-11-23 12:30:01 +0600

    correct commit 4

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from rest_framework.request import Request
